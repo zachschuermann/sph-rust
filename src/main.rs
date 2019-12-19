3169fbc4ad03d118d98ad57b062ac4e1ff78d962
Zach Schuermann <zachary.zvs@gmail.com>
2019-12-19 14:26:05 -0600

    idk need to investigate openGL bindings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    let el = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_title("Hello world!")
+        .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+    let windowed_context = glutin::ContextBuilder::new()
+        .build_windowed(wb, &el)
+        .unwrap();
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
 }
